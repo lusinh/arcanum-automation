bc2a716a3c65c30130785bbd43b7ce28298ae6a6
Harrygiel <harrygiel@gmail.com>
2020-01-14 10:41:31 +0100

    correcting name, url and version

--- automate.user.js
+++ automate.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         aardvark arcanum auto
-// @version      1358.1
+// @name         aardvark arcanum auto - Harrygiel's fork
+// @version      1358
 // @author       aardvark, Linspatz
 // @description  Automates casting buffs, buying gems making types gems, making lore. Adds sell junk/dupe item buttons. Must open the main tab and the spells tab once to work.
 // @downloadURL  https://github.com/mettalogic/arcanum-automation/raw/master/automate.user.js
-// @match        http://www.lerpinglemur.com/arcanum/*
+// @match        *//www.lerpinglemur.com/arcanum/*
 // @match        http://game312933.konggames.com/gamez/0031/2933/*
 // @match        https://game312933.konggames.com/gamez/0031/2933/*
 // @run-at       document-idle
